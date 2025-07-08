ccdad36d7dadf245acebb37dcce756c5d3d9719f
Leticia <giovannaleticia02203@gmail.com>
2025-07-08 18:50:16 -0300

    configurando os relacionamentos de autor

--- biblioteca_project/app/Models/Autor.php
+++ biblioteca_project/app/Models/Autor.php
@@ -8,4 +8,17 @@ class Autor extends Model
 {
     protected $table = 'autores';
     protected $fillable = ['nome', 'data_nascimento', 'biografia'];
+
+    public function categoria(){
+        return $this->belongsTo(Categoria::class);
+    }
+
+    public function editora(){
+        return $this->belongsTo(Editora::class);
+    }
+
+    public function exemplares(){
+        return $this->hasMany(Exemplar::class);
+    }
+
 }
