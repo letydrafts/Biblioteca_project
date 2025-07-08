34cdee0c16a5f0a1d97c0d6271441037b67879fe
Leticia <giovannaleticia02203@gmail.com>
2025-07-08 20:00:32 -0300

    configurando os relacionamentos da model Livro

--- app/Models/Livro.php
+++ app/Models/Livro.php
@@ -8,4 +8,20 @@ class Livro extends Model
 {
     protected $table = 'livros';
     protected $fillable = ['titulo', 'isbn', 'páginas', 'sinopse', 'editora_id', 'categoria_id'];
+
+    public function categoria(){
+        return $this->belongsTo(Categoria::class);
+    }
+
+    public function editora(){
+        return $this->belongsTo(Editora::class);
+    }
+
+    public function exemplares(){
+        return $this->hasMany(Exemplares::class);
+    }
+
+    public function autores(){
+        return $this->belongsToMany(Autor::class);
+    }
 }
