1b79192b510be6ce2eecb0408f6174f314dc16bb
Leticia <giovannaleticia02203@gmail.com>
2025-07-08 21:43:17 -0300

    alterações na migração de users

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->date('data_nascimento');
+            $table->string('cpf');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
