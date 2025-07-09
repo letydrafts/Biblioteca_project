015a54bcef24194a4b2907ca47c13030283c0aa2
Leticia <giovannaleticia02203@gmail.com>
2025-07-08 21:49:05 -0300

    criando migration de reservas

---
+++ database/migrations/2025_07_09_004346_create_reservas_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reservas', function (Blueprint $table) {
+            $table->id()->autoIncrement();
+            $table->date('data_reserva');
+            $table->string('status');
+            $table->foreignForId(User::class)->constrained()->onDelete('cascade');
+            $table->foreignForId(Exemplar::class)->constrained()->onDelete('cascade');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reservas');
+    }
+};
