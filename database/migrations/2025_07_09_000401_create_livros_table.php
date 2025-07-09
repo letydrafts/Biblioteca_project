364223c587c9985a46d6657b151f3ffdb3094e53
Leticia <giovannaleticia02203@gmail.com>
2025-07-08 22:31:02 -0300

    incluindo as importações que faltaram nas migrações

--- database/migrations/2025_07_09_000401_create_livros_table.php
+++ database/migrations/2025_07_09_000401_create_livros_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Editora;
+use App\Models\Categoria;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -14,7 +16,7 @@ return new class extends Migration
         Schema::create('livros', function (Blueprint $table) {
             $table->id()->autoIncrement();
             $table->string('nome');
-            $table->int('isbn');
+            $table->integer('isbn');
             $table->ForeignIdFor(Editora::class)->constrained()->onDelete('cascade');
             $table->ForeignIdFor(Categoria::class)->constrained()->onDelete('cascade');
             $table->softDeletes();
--- database/migrations/2025_07_09_003733_create_exemplares_table.php
+++ database/migrations/2025_07_09_003733_create_exemplares_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Livros;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -16,7 +17,7 @@ return new class extends Migration
             $table->integer('codigo');
             $table->string('status');
             $table->softDeletes();
-            $table->fforeignIdFor(Livro::class)->contrained()->onDelete('cascade');
+            $table->foreignIdFor(Livro::class)->contrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
