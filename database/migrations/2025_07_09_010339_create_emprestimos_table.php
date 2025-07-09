bc6e9bcb80adc46956cd1161a34c1d25e535a8bf
Leticia <giovannaleticia02203@gmail.com>
2025-07-08 22:06:23 -0300

    criando migration de emprestimos

---
+++ database/migrations/2025_07_09_010339_create_emprestimos_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('emprestimos', function (Blueprint $table) {
+            $table->id()->autoIncrement();
+            $table->date('data_empréstimo');
+            $table->date('data_devolucao');
+            $table->foreignForId(User::class)->constrained()->onDelete('cascade');
+            $table->foreignForId(Exemplar::class)->constrained()->onDelete('cascade');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('emprestimos');
+    }
+};
