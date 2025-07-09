b622a484bb95bfca7d207de7ede007e43754cdb1
Leticia <giovannaleticia02203@gmail.com>
2025-07-08 21:09:24 -0300

    criando migration de livros

---
+++ database/migrations/2025_07_09_000401_create_livros_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('livros', function (Blueprint $table) {
+            $table->id()->autoIncrement();
+            $table->string('nome');
+            $table->int('isbn');
+            $table->ForeignIdFor(Editora::class)->constrained()->onDelete('cascade');
+            $table->ForeignIdFor(Categoria::class)->constrained()->onDelete('cascade');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('livros');
+    }
+};
